c3607cbfc7ab3340bff75c2bdea4be702c8bddfc
Kanishka Wijesekara <123kanishka@gmail.com>
2021-04-26 19:07:30 +0530

    Update read_metadata.sh

--- read_metadata.sh
+++ read_metadata.sh
@@ -2,5 +2,14 @@
 input="metadata.toml"
 while IFS= read -r line
 do
-  echo "$line"
+#   echo "$line"
+  arrIN=(${line//=/ })
+  key=${arrIN[0]}
+  value=${arrIN[1]}   
+  declare $key=$value
 done < "$input"
+
+echo "$dirname"
+echo "$version"
+echo "$name"
+echo "$path"
